b3e9aa07e70b99dd47471727c869f7431f4e42e8
alexdelmed <alexdm14@live.com.mx>
2018-02-08 20:49:24 -0600

    practica4

---
+++ practicas/pract3/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int numberOfSegments;
+  int totalTime;
+
+  int *distances;
+  int *speeds;
+
+  scanf("%d %d",&numberOfSegments, &totalTime);
+
+  distances = (int *)malloc(sizeof(int)*numberOfSegments);
+  speeds = (int *)malloc(sizeof(int)*numberOfSegments);
+
+  for (int i = 0; i < numberOfSegments; i++)
+  {
+    scanf("%d %d", &distances[i], &speeds[i]);
+  }
+
+  //Print the inputs
+  /*
+  printf("%d %d \n", numberOfSegments,totalTime);
+  for (int i = 0; i < numberOfSegments; i++)
+  {
+    printf("%d %d \n",distances[i], speeds[i]);
+  }
+  free(distances);
+  free(speeds);
+  */
+
+  return 0;
+}
